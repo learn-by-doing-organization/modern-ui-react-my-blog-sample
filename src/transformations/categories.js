ebd4cafda62bfa9c897673ebdb90837b71635eb3
magicianred <magicianred@gmail.com>
2021-05-09 12:03:56 +0200

    added transformation function for categories

---
+++ src/transformations/categories.js
@@ -0,0 +1,34 @@
+import { USE_DUMMY_DATA } from '../enums/categories';
+
+/**
+ * convert data for send to backend
+ */
+export const transformToAPI = ((data) => {
+  if(USE_DUMMY_DATA) {
+    return data;
+  } else {
+    return data;
+  }
+});
+
+/**
+ * convert data for details
+ */
+export const transformToDetails = ((data) => {
+  if(USE_DUMMY_DATA) {
+    return data;
+  } else {
+    return data;
+  }
+});
+
+/**
+ * convert data for list view
+ */
+export const transformToList = ((data) => {
+    if(USE_DUMMY_DATA) {
+      return data;
+    } else {
+      return data;
+    }
+});
